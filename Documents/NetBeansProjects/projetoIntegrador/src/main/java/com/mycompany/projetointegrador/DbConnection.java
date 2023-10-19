a000853797853dcd03a145f520361a50a91630f3
Elias <eliasmanica@poli.ufrj.br>
2023-10-19 20:26:39 -0300

    separanco conexão banco

---
+++ Documents/NetBeansProjects/projetoIntegrador/src/main/java/com/mycompany/projetointegrador/DbConnection.java
@@ -0,0 +1,30 @@
+package com.mycompany.projetointegrador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String usuario = "postgres";
+    private static final String senha = "postgres";
+    private Connection conexao;
+
+    public void DatabaseConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão realizada com sucesso!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Erro caso o driver JDBC não tenha sido instalado");
+        } catch (SQLException e) {
+            System.out.println("Erro caso haja problemas para se conectar ao banco de dados: " + e.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        System.out.println("Conectando ao banco");
+        this.DatabaseConnection();
+        return conexao;
+    }
+}
--- Documents/NetBeansProjects/projetoIntegrador/src/main/java/com/mycompany/projetointegrador/ProjetoIntegrador.java
+++ Documents/NetBeansProjects/projetoIntegrador/src/main/java/com/mycompany/projetointegrador/ProjetoIntegrador.java
@@ -1,33 +1,31 @@
 package com.mycompany.projetointegrador;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ProjetoIntegrador {
-
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String usuario = "postgres";
-        String senha = "postgres";
-        
-        System.out.println("Hello World!");
-        try {
-            Class.forName("org.postgresql.Driver");
-            Connection conexao = DriverManager.getConnection(url, usuario, senha);
-            
-            System.out.println("Conexão realizada com sucesso!");
-            
-            ResultSet rsClient = conexao.createStatement().executeQuery("SELECT * FROM public.bicicleta");
-        
-            while(rsClient.next()) {
-                System.out.println("Código!" + rsClient.getString("codigo"));
+        DbConnection dataConnection = new DbConnection();
+        Connection conexao = dataConnection.getConnection();
+
+        if (conexao != null) {
+            try {
+                Statement statement = conexao.createStatement();
+                 System.out.println("Banco conectado");
+                  System.out.println("----------------");
+                ResultSet rsClient = statement.executeQuery("SELECT * FROM public.bicicleta");
+
+                while (rsClient.next()) {
+                    System.out.println("Código: " + rsClient.getString("codigo"));
+                }
+            } catch (SQLException e) {
+                System.out.println("Erro ao executar a consulta: " + e.getMessage());
             }
-        } catch (ClassNotFoundException e) {
-            System.out.println("Erro caso o driver JDBC não foi instalado");
-        } catch (SQLException e) {
-            System.out.println("Erro caso haja problemas para se conectar ao banco de dados" + e.getMessage());
+        } else {
+            System.out.println("Não foi possivel conectar ao banco");
         }
     }
+    
 }
